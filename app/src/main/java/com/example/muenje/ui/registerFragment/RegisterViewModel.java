344ebfd9c1cef5ced688ab7d6f5d990eca279419
unknown <ibudos@etfos.hr>
2020-09-23 17:27:47 +0200

    fragment_register.xml made

--- app/src/main/java/com/example/muenje/ui/registerFragment/RegisterViewModel.java
+++ app/src/main/java/com/example/muenje/ui/registerFragment/RegisterViewModel.java
@@ -1,4 +1,14 @@
 package com.example.muenje.ui.registerFragment;
 
-public class RegisterViewModel {
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.muenje.core.RxViewModel;
+
+public class RegisterViewModel extends RxViewModel {
+
+    public MutableLiveData<String> mUsername = new MutableLiveData<>("");
+    public MutableLiveData<String> mPassword = new MutableLiveData<>("");
+    public MutableLiveData<String> mRepeatPassword = new MutableLiveData<>("");
+
 }
